f8740ad99d3b99f82229c1417812af82eeccda0d
Devensh22345 <kumardevensh0@gmail.com>
2025-03-01 18:19:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ API_HASH = os.getenv("API_HASH", "5c0ad7c48a86afac87630ba28b42560d")
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Test:Test@cluster0.pcpx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7519708626:AAEYmd7k71S1t9db4ZN6fIl7alDxhtWO1ic")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7519708626:AAEY8r2HnXbUDPeySmj6qWkMqlJJmsnimGk")
 
 LINK_CHANGE_INTERVAL = 6 * 60 * 60  # 6 hours in seconds
